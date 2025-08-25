a5b74e995346b82f1dd41a0806300639bd70eb27
lazymio <mio@lazym.io>
2025-08-25 14:10:43 +0800

    table orm design

--- crates/mdbx-derive-traits/src/error.rs
+++ crates/mdbx-derive-traits/src/error.rs
@@ -12,54 +12,26 @@ pub enum MDBXDeriveError {
     #[error("corrputed")]
     Corrupted,
     #[error("JSON: {0}")]
-    JSON(JSONError),
+    JSON(#[from] JSONError),
     #[error("zstd: {0}")]
-    Zstd(std::io::Error),
+    Zstd(#[from] std::io::Error),
     #[error("bincode encode: {0}")]
-    BincodeEncode(bincode::error::EncodeError),
+    BincodeEncode(#[from] bincode::error::EncodeError),
     #[error("bincode decode: {0}")]
-    BincodeDecode(bincode::error::DecodeError),
+    BincodeDecode(#[from] bincode::error::DecodeError),
     #[error("mdbx: {0}")]
-    MDBX(libmdbx_remote::Error),
+    MDBX(#[from] libmdbx_remote::Error),
+    #[error("mdbx-remote: {0}")]
+    Client(libmdbx_remote::ClientError),
     #[error("bcs: {0}")]
-    BCS(bcs::Error),
+    BCS(#[from] bcs::Error),
 }
 
-#[cfg(feature = "serde_json")]
-impl From<serde_json::Error> for MDBXDeriveError {
-    fn from(value: serde_json::Error) -> Self {
-        Self::JSON(value)
-    }
-}
-
-#[cfg(feature = "simd-json")]
-impl From<simd_json::Error> for MDBXDeriveError {
-    fn from(value: simd_json::Error) -> Self {
-        Self::JSON(value)
-    }
-}
-
-#[cfg(all(not(feature = "simd-json"), not(feature = "serde_json")))]
-impl From<String> for MDBXDeriveError {
-    fn from(value: String) -> Self {
-        Self::JSON(value)
-    }
-}
-
-impl From<bincode::error::EncodeError> for MDBXDeriveError {
-    fn from(value: bincode::error::EncodeError) -> Self {
-        Self::BincodeEncode(value)
-    }
-}
-
-impl From<bincode::error::DecodeError> for MDBXDeriveError {
-    fn from(value: bincode::error::DecodeError) -> Self {
-        Self::BincodeDecode(value)
-    }
-}
-
-impl From<bcs::Error> for MDBXDeriveError {
-    fn from(value: bcs::Error) -> Self {
-        Self::BCS(value)
+impl From<libmdbx_remote::ClientError> for MDBXDeriveError {
+    fn from(value: libmdbx_remote::ClientError) -> Self {
+        match value {
+            libmdbx_remote::ClientError::MDBX(e) => Self::MDBX(e),
+            _ => Self::Client(value),
+        }
     }
 }
